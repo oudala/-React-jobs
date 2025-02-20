e81cbb69d07d782ca82513cf111987c306af26b7
oudala <ilyassouladdahman@gmail.com>
2025-02-20 02:44:00 +0100

    Finalize project and prepare for deployment

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import HomeCards from '../components/HomeCards'
-import JobListings from '../components/JobListings'
-import ViewAllJobs from '../components/ViewAllJobs'
+import Hero from '../components/Hero';
+import HomeCards from '../components/HomeCards';
+import JobListings from '../components/JobListings';
+import ViewAllJobs from '../components/ViewAllJobs';
 
-function HomePage() {
+const HomePage = () => {
   return (
     <>
-    <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs" />
-    <HomeCards />
-    <JobListings isHome={true} />
-    <ViewAllJobs />
+      <Hero />
+      <HomeCards />
+      <JobListings isHome={true} />
+      <ViewAllJobs />
     </>
-  )
-}
-
-export default HomePage
+  );
+};
+export default HomePage;
--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -7,4 +7,5 @@ const JobsPage = () => {
     </section>
   );
 };
+
 export default JobsPage;
